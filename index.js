86f3a99dc144926a362731077804608599665fcf
serendiptea <142447859+serendiptea@users.noreply.github.com>
2023-08-29 23:06:15 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-// Search Bar Function
-const searchInput = document.querySelector("[data-search]");
-const searchButton = document.getElementById("search_button");
-const articleCards = document.querySelectorAll(".article_cards");
-
-searchButton.addEventListener("click", () => {
-    const searchTerm = searchInput.value.toLowerCase(); // Get the user's input and convert it to lowercase for case-insensitive comparison
-
-articleCards.forEach(card => {
-    const cardText = card.textContent.toLowerCase(); // Get the card's text and convert it to lowercase
-    if (cardText.includes(searchTerm)) {
-    card.style.display = "block"; // Show the card if it contains the search term
-    } else {
-    card.style.display = "none"; // Hide the card if it doesn't contain the search term
-    }
-});
-
-// Clear the search input field after the search is performed
-    searchInput.value = "";
-});
-
 // Newsletter Lists
+// function returnEmail() {
+    //let input = document.getElementById("email_input").value
+    //alert(input)
+//}
+
 function returnEmail() {
-    let input = document.getElementById("email_input").value
-    alert(input)
+    // Get the email input value
+    let input = document.getElementById("email_input").value;
+
+    // Check if localStorage already has an array of emails
+    let savedEmails = JSON.parse(localStorage.getItem("newsletterEmails")) || [];
+
+    // Add the new email to the array
+    savedEmails.push(input);
+
+    // Save the updated array back to localStorage
+    localStorage.setItem("newsletterEmails", JSON.stringify(savedEmails));
+
+    // Optionally, provide some feedback to the user
+    alert("Thank you for subscribing!");
+
+    // Clear the input field
+    document.getElementById("email_input").value = "";
+}
+
+// Retrieve the stored email addresses
+let storedEmails = JSON.parse(localStorage.getItem("newsletterEmails"));
+
+if (storedEmails && storedEmails.length > 0) {
+    // If there are stored email addresses, you can iterate through them and do something with them.
+    for (let i = 0; i < storedEmails.length; i++) {
+        console.log("Email " + (i + 1) + ": " + storedEmails[i]);
+        // You can also display them in your HTML if desired.
+        // For example, you can create a div and append the email addresses to it.
+        // Replace 'outputDiv' with the ID of the HTML element where you want to display the emails.
+        // document.getElementById('outputDiv').innerHTML += "<p>Email " + (i + 1) + ": " + storedEmails[i] + "</p>";
+    }
+} else {
+    console.log("No email addresses stored in localStorage.");
 }
